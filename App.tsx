57ef7580d5944b98e97b81e587235ceccf971601
yuno-src <example@ex.com>
2024-08-10 09:34:07 -0500

    list, styles, hamburger

--- App.tsx
+++ App.tsx
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import ReadingList from './components/ReadingList';
-import React from 'react';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ReadingList />
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
