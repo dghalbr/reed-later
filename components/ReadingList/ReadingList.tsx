7116cde44db9623c1d6ec21012ab4fd99892f7ee
curt-codes <curtcodes85@gmail.com>
2024-08-11 15:54:26 -0500

    add new line item when pressing button

--- components/ReadingList/ReadingList.tsx
+++ components/ReadingList/ReadingList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   ScrollView,
   Text,
@@ -20,23 +20,26 @@ const data: IReedDataEntry[] = [
   { url: "http://example.com/" },
 ];
 
-const buttonsListArr = data.map((entry: IReedDataEntry) => (
-  <>
-    <TouchableOpacity key={entry.url} onPress={() => Linking.openURL(entry.url)}>
-      <Text style={componentStyles.listItem} key={entry.url}>
-        {entry.url}
-      </Text>
-      {entry != data[data.length - 1] && <View style={componentStyles.horizontalRule} />}
-    </TouchableOpacity>
-  </>
-));
-
-
-function buttonClick(event: GestureResponderEvent): void {
-  console.log("Button Logic here");
-} 
-
 const ReadingList = () => {
+  const [listData, setListData] = useState<IReedDataEntry[]>(data);
+
+  function buttonClick(event: GestureResponderEvent): void {
+    const myNewUrl: IReedDataEntry[] = [{
+      url: "https://www.apple.com",
+    }];
+  
+    setListData([...listData, ...myNewUrl]);
+  } 
+
+  const buttonsListArr = listData.map((entry: IReedDataEntry) => (
+      <TouchableOpacity key={entry.url} onPress={() => Linking.openURL(entry.url)}>
+        <Text style={componentStyles.listItem} key={entry.url}>
+          {entry.url}
+        </Text>
+        {entry != data[data.length - 1] && <View style={componentStyles.horizontalRule} />}
+      </TouchableOpacity>
+  ));
+
   return (
     <View style={componentStyles.container}>
       <Feather
